f11cea7dcf119f3646b88c212b728469d84da205
Dustin Gardner <drgardner42@students.tntech.edu>
2016-05-03 09:18:00 -0700

    Fixed a bug in getStuff, removed absolute pathing

--- getStuff.sh
+++ getStuff.sh
@@ -1,11 +1,13 @@
 mkdir Apps
+num=1
 while read line
 do
    url=`echo "$line" | grep -o "https[^\"]*"`
    name=`echo "$line" | awk -F ":" '{print $4}' | cut -c4- | rev | cut -c5- | rev`
-   mkdir "/home/drgardner/School/FDroid/Apps/${name}"
-   cd /home/drgardner/School/FDroid/Apps
-   cd "$name"
+   mkdir "Apps/${name}" || mkdir "Apps/${num}"
+   cd Apps/"${name}" || cd Apps/"${num}"
    wget --tries=45 "$url"
+   cd ../..
    sleep $[ ( $RANDOM % 10 ) + 1 ]s
+   num=$((num + 1))
 done < urls.json
